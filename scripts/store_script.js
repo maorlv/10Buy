153899ccc3d890fb4e4d7ac396fa56b6bd4707a4
Maor Levy <maorlevy@gmail.com>
2019-05-15 23:18:36 +0300

    massive update...

---
+++ scripts/store_script.js
@@ -0,0 +1,45 @@
+// get the button
+var btn = document.querySelector(".google-extension-connect");
+if (btn) {
+    chrome.storage.local.get({"stores_info": []}, (res)=>{
+
+        // check if current store is already configured in the extension
+        var already_configured = false;
+        res["stores_info"].forEach((store) => {
+            if (store["site_url"] == window.location.hostname) {
+                already_configured = true;
+                break; // no need to check others...
+            }
+        });
+
+        var btn_click = function() {
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", "https://" + window.location.hostname + "/admin/api/getToken", true);
+            xhr.responseType = "json";
+            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4)
+                    if (xhr.status === 200) {
+                        // add the store to the current stores-list and then save the new list
+                        res["stores_info"].push(xhr.response);
+                        chrome.storage.local.set({"stores_info": res["stores_info"]});
+
+                        // tell the user that the connection was successful
+                        btn.querySelector(".btn").textContent = "חיבור בוצע בהצלחה";
+
+                        // we don't want the user to click again and add the same store more than once...
+                        btn.removeEventListener("click", btn_click);
+                    }
+            };
+        
+            xhr.send();
+        };
+
+        // if not configured, show the button the user and assign btn_click() to the button
+        if (!already_configured) {
+            btn.style.display = "inline-block";
+            btn.addEventListener("click", btn_click);
+        }
+    });
+}
